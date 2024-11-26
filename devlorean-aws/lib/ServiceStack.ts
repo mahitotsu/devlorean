59b441225bcf069cfed678750303b5662de4a360
Akio Kanashiki <akio@mail.com>
2024-11-27 05:35:00 +0900

    modified

--- devlorean-aws/lib/ServiceStack.ts
+++ devlorean-aws/lib/ServiceStack.ts
@@ -1,5 +1,7 @@
 import { Duration, Stack, StackProps } from "aws-cdk-lib";
-import { ApplicationLoadBalancer, ApplicationProtocol, ApplicationTargetGroup, IApplicationLoadBalancer, TargetType } from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import { Peer, Port } from "aws-cdk-lib/aws-ec2";
+import { Cluster, ContainerImage, CpuArchitecture, FargateService, FargateTaskDefinition } from "aws-cdk-lib/aws-ecs";
+import { ApplicationLoadBalancer, ApplicationProtocol, ApplicationTargetGroup, IApplicationLoadBalancer, ListenerCondition, TargetType } from "aws-cdk-lib/aws-elasticloadbalancingv2";
 import { LambdaTarget } from "aws-cdk-lib/aws-elasticloadbalancingv2-targets";
 import { Architecture, Runtime } from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
@@ -18,13 +20,30 @@ export class ServiceStack extends Stack {
         const loadBalancer = ApplicationLoadBalancer.fromApplicationLoadBalancerAttributes(this, 'LoadBalancer', {
             loadBalancerArn: props.loadBalancer.loadBalancerArn,
             securityGroupId: props.loadBalancer.connections.securityGroups[0].securityGroupId,
+            vpc: props.loadBalancer.vpc,
         });
+        const cluster = new Cluster(this, 'ServiceCluster', { vpc: loadBalancer.vpc, });
 
         const serviceHealthCheck = new NodejsFunction(this, 'ServiceHealthCheck', {
             runtime: Runtime.NODEJS_20_X,
             architecture: Architecture.ARM_64,
             entry: `${__dirname}/functions/ServiceHealthCheck.ts`,
         });
+
+        const serviceWebTaskDefinition = new FargateTaskDefinition(this, 'ServiceWebTaskDefintiion', {
+            cpu: 512, memoryLimitMiB: 1024,
+            runtimePlatform: { cpuArchitecture: CpuArchitecture.X86_64 },
+        });
+        const serviceWebContainer = serviceWebTaskDefinition.addContainer('Web', {
+            image: ContainerImage.fromAsset(`${__dirname}/../../devlorean-web`),
+            portMappings: [{ containerPort: 3000 }],
+        });
+        const serviceWeb = new FargateService(cluster, 'ServiceWeb', {
+            taskDefinition: serviceWebTaskDefinition,
+            cluster,
+            desiredCount: 0,
+        });
+        loadBalancer.connections.allowTo(Peer.ipv4(cluster.vpc.vpcCidrBlock), Port.tcp(serviceWebContainer.portMappings[0].containerPort));
 
         const listener = loadBalancer.addListener('Listener', {
             port: props.listenerPort,
@@ -41,6 +60,28 @@ export class ServiceStack extends Stack {
                     timeout: Duration.seconds(3),
                 },
             })]
+        });
+        listener.addTargetGroups('Web', {
+            priority: 100,
+            conditions: [
+                ListenerCondition.pathPatterns(['/*']),
+            ],
+            targetGroups: [new ApplicationTargetGroup(serviceWeb, 'TargetGroup', {
+                targetType: TargetType.IP,
+                vpc: cluster.vpc,
+                port: serviceWebContainer.portMappings[0].containerPort,
+                protocol: ApplicationProtocol.HTTP,
+                targets: [serviceWeb],
+                healthCheck: {
+                    enabled: true,
+                    path: '/',
+                    healthyHttpCodes: '200',
+                    healthyThresholdCount: 2,
+                    unhealthyThresholdCount: 2,
+                    interval: Duration.seconds(10),
+                    timeout: Duration.seconds(3),
+                }
+            })],
         });
     }
 }
---
+++ devlorean-web/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>Welcome</h1>
+        <p>Hello world. This is an index page.</p>
+    </div>
+</template>
