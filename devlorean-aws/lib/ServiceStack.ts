192c18438ccb97ebe1949223595c9db086516944
Akio Kanashiki <akio@mail.com>
2024-11-27 09:01:38 +0900

    modifeid

--- devlorean-aws/lib/ServiceStack.ts
+++ devlorean-aws/lib/ServiceStack.ts
@@ -1,5 +1,5 @@
 import { Duration, Stack, StackProps } from "aws-cdk-lib";
-import { Peer, Port } from "aws-cdk-lib/aws-ec2";
+import { Peer, Port, SubnetType } from "aws-cdk-lib/aws-ec2";
 import { Cluster, ContainerImage, CpuArchitecture, FargateService, FargateTaskDefinition } from "aws-cdk-lib/aws-ecs";
 import { ApplicationLoadBalancer, ApplicationProtocol, ApplicationTargetGroup, IApplicationLoadBalancer, ListenerCondition, TargetType } from "aws-cdk-lib/aws-elasticloadbalancingv2";
 import { LambdaTarget } from "aws-cdk-lib/aws-elasticloadbalancingv2-targets";
@@ -30,20 +30,26 @@ export class ServiceStack extends Stack {
             entry: `${__dirname}/functions/ServiceHealthCheck.ts`,
         });
 
+        const containerPort = 3000;
         const serviceWebTaskDefinition = new FargateTaskDefinition(this, 'ServiceWebTaskDefintiion', {
             cpu: 512, memoryLimitMiB: 1024,
             runtimePlatform: { cpuArchitecture: CpuArchitecture.X86_64 },
         });
         const serviceWebContainer = serviceWebTaskDefinition.addContainer('Web', {
-            image: ContainerImage.fromAsset(`${__dirname}/../../devlorean-web`),
-            portMappings: [{ containerPort: 3000 }],
+            image: ContainerImage.fromAsset(`${__dirname}/../../devlorean-web`, {
+                assetName: `ContainerImage${Date.now()}`
+            }),
+            portMappings: [{ containerPort }],
         });
         const serviceWeb = new FargateService(cluster, 'ServiceWeb', {
             taskDefinition: serviceWebTaskDefinition,
             cluster,
-            desiredCount: 0,
+            desiredCount: 1,
+            vpcSubnets: { subnetType: SubnetType.PRIVATE_WITH_EGRESS },
         });
-        loadBalancer.connections.allowTo(Peer.ipv4(cluster.vpc.vpcCidrBlock), Port.tcp(serviceWebContainer.portMappings[0].containerPort));
+        cluster.vpc.selectSubnets({ subnetType: SubnetType.PRIVATE_WITH_EGRESS }).subnets.forEach(subnet => {
+            loadBalancer.connections.allowTo(Peer.ipv4(subnet.ipv4CidrBlock), Port.tcp(containerPort));
+        });
 
         const listener = loadBalancer.addListener('Listener', {
             port: props.listenerPort,
