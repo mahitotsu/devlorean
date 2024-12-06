c9ce5433ea74e316b87c8bba06de0f09b73e18d1
Akio Kanashiki <akio@mail.com>
2024-12-07 00:37:33 +0900

    modified

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-bun run --cwd ./devlorean-web build
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+bun run --cwd $SCRIPT_DIR/devlorean-web build
+mvn -f $SCRIPT_DIR/devlorean-api package
--- devlorean-aws/lib/DevloreanStack.ts
+++ devlorean-aws/lib/DevloreanStack.ts
@@ -1,76 +1,50 @@
-import { CfnOutput, Duration, Stack, StackProps } from "aws-cdk-lib";
-import { IpAddresses, IpProtocol, Ipv6Addresses, Peer, Port, SecurityGroup, SubnetType, Vpc } from "aws-cdk-lib/aws-ec2";
-import { Cluster, ContainerImage, FargateService, FargateTaskDefinition } from "aws-cdk-lib/aws-ecs";
-import { IpAddressType, NetworkLoadBalancer, NetworkTargetGroup, Protocol } from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import { Stack, StackProps } from "aws-cdk-lib";
+import { IpAddresses, IpProtocol, Ipv6Addresses, SubnetType, Vpc } from "aws-cdk-lib/aws-ec2";
+import { Cluster, ContainerImage, FargateService, FargateTaskDefinition, ICluster } from "aws-cdk-lib/aws-ecs";
+import { NamespaceType } from "aws-cdk-lib/aws-servicediscovery";
 
 export class DevloreanStack extends Stack {
     constructor(scope: any, id: string, props: StackProps) {
         super(scope, id, props);
 
-        const webServerPort = 3001;
-        const listenerPort = 80;
-
         const vpc = new Vpc(this, 'Vpc', {
-            createInternetGateway: true,
-            maxAzs: 2, natGateways: 2,
+            maxAzs: 2, createInternetGateway: true, natGateways: 2,
             ipProtocol: IpProtocol.DUAL_STACK,
-            ipAddresses: IpAddresses.cidr('10.0.0.0/24'),
-            ipv6Addresses: Ipv6Addresses.amazonProvided(),
+            ipAddresses: IpAddresses.cidr('10.0.0.0/24'), ipv6Addresses: Ipv6Addresses.amazonProvided(),
             subnetConfiguration: [{
-                name: 'public', subnetType: SubnetType.PUBLIC,
-                cidrMask: 28, ipv6AssignAddressOnCreation: true,
+                name: 'Public', subnetType: SubnetType.PUBLIC, cidrMask: 27,
             }, {
-                name: 'private', subnetType: SubnetType.PRIVATE_WITH_EGRESS,
-                cidrMask: 26, ipv6AssignAddressOnCreation: true,
-            }],
+                name: 'Private', subnetType: SubnetType.PRIVATE_WITH_EGRESS, cidrMask: 26,
+            }, {
+                name: 'Isolated', subnetType: SubnetType.PRIVATE_ISOLATED, cidrMask: 27,
+            }]
         });
 
-        const lbSg = new SecurityGroup(this, 'LoadBalancerSecurityGroup', {
-            vpc, allowAllOutbound: false, allowAllIpv6Outbound: false,
+        const cluster = new Cluster(vpc, 'Cluster', {
+            vpc,
+            defaultCloudMapNamespace: { name: 'devlorean.local', useForServiceConnect: true, type: NamespaceType.DNS_PRIVATE },
         });
-        const lb = new NetworkLoadBalancer(vpc, 'NetworkLoadBalancer', {
-            internetFacing: true,
-            vpc, vpcSubnets: { subnetType: SubnetType.PUBLIC },
-            ipAddressType: IpAddressType.DUAL_STACK,
-            securityGroups: [lbSg],
-        });
-        const listener = lb.addListener('Listener', {
-            port: listenerPort, protocol: Protocol.TCP,
-        });
-        lb.connections.allowFrom(Peer.anyIpv4(), Port.tcp(listenerPort));
-        lb.connections.allowFrom(Peer.anyIpv6(), Port.tcp(listenerPort));
 
-        const serviceCluster = new Cluster(vpc, 'ServiceCluster', { vpc, });
+        this.createFargateService('Web', cluster, 256, 512, `${__dirname}/../../devlorean-web`, 3000, 1);
+        this.createFargateService('Api', cluster, 512, 1024, `${__dirname}/../../devlorean-api`, 9080, 1);
+    }
 
-        const webServerTaskDefinition = new FargateTaskDefinition(this, 'WebServerTaskDefinition', {
-            cpu: 256, memoryLimitMiB: 512,
-        });
-        webServerTaskDefinition.addContainer('WebServerContainer', {
-            image: ContainerImage.fromAsset(`${__dirname}/../../devlorean-web`, {
-                buildArgs: { port: webServerPort.toString(), }
-            }),
-            portMappings: [{ containerPort: webServerPort }]
-        });
-        const webServerService = new FargateService(serviceCluster, 'WebServerService', {
-            taskDefinition: webServerTaskDefinition,
-            cluster: serviceCluster, vpcSubnets: { subnetType: SubnetType.PRIVATE_WITH_EGRESS },
-            assignPublicIp: false,
-            desiredCount: 1,
-        });
-        const webServerTargetGroup = new NetworkTargetGroup(vpc, 'WebServerTargetGroup', {
-            vpc, port: webServerPort,
-            targets: [webServerService],
-            healthCheck: {
-                enabled: true,
-                port: webServerPort.toString(), protocol: Protocol.TCP,
-                healthyThresholdCount: 2, unhealthyThresholdCount: 2,
-                timeout: Duration.seconds(2), interval: Duration.seconds(10),
-            },
-            deregistrationDelay: Duration.seconds(0),
-        });
-        listener.addTargetGroups('WebServerTargetGroup', webServerTargetGroup);
-        webServerService.connections.allowFrom(lbSg, Port.tcp(webServerPort));
+    createFargateService(name: string, cluster: ICluster, cpu: number, memory: number, assetPath: string, port: number, desiredCount: number) {
 
-        new CfnOutput(this, 'EntryPointURL', { value: `http://${lb.loadBalancerDnsName}:${listenerPort}` });
+        const taskDefinition = new FargateTaskDefinition(cluster, `${name}TaskDefinition`, {
+            cpu, memoryLimitMiB: memory,
+        });
+        taskDefinition.addContainer(`${name}Container`, {
+            image: ContainerImage.fromAsset(assetPath),
+            portMappings: [{ containerPort: port, name: name.toLowerCase() }],
+        });
+
+        const service = new FargateService(cluster, `${name}Service`, {
+            taskDefinition, cluster, assignPublicIp: false,
+            vpcSubnets: { subnetType: SubnetType.PRIVATE_WITH_EGRESS },
+            serviceConnectConfiguration: { services: [{ portMappingName: name.toLowerCase(), }] },
+            desiredCount,
+        });
+        return service;
     }
 }
